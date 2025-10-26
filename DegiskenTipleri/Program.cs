fc089d3a5d9ea03a50f75eb0ffc6b4d216d0ebfe
metndurn <mtndrn123@gmail.com>
2025-10-26 22:07:33 +0300

    Add project files.

---
+++ DegiskenTipleri/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegiskenTipleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte s1 = 10;
+            short s2 = 20;
+            int s3 = 30;
+            long s4 = 40;
+            float s5 = 50.2f;
+            double s6 = 60;
+            char karakter = 'A';
+            string metin = "Metin Duran";
+            bool yes=false;
+            object a = true;
+
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(karakter);
+            Console.WriteLine(metin);
+            Console.WriteLine(yes);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Operatorler/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			#region toplama islemi
+			//int sayi1 = 12, sayi2 = 50;
+			//int topla=sayi1 + sayi2;
+
+			//Console.WriteLine("Toplama islemi:" + topla);
+			//Console.WriteLine("********");
+
+			//int s1 = 50, s2 = 45;
+			//int eksi = s1 - s2;
+			//Console.WriteLine("Cikarma islemi:" + eksi);
+			#endregion
+
+			#region cıkarma islemi
+			//int s1 = 42, s2 = 12;
+			//int carp = s1 * s2;
+			//Console.WriteLine("Carpma islemi:" + carp);
+			//Console.WriteLine("**********");
+
+			//int s3 = 55, s4 = 45;
+			//int bolme = s3 / s4;
+			//Console.WriteLine("Bolme islemi:" + bolme);
+			#endregion
+
+			#region mod alma islemi
+			//bir sayinin diger sayiya bolumunden kalani verir
+			//int sayi1 = 10, sayi2 = 3;
+
+			//int mod = sayi1 % sayi2;
+
+			//Console.WriteLine("Mod alma islemi:" + mod);
+			#endregion
+
+			#region artirma ve azaltma islemi
+			//int sayi = 10;
+			//sayi++; // + isareti ile var olan sayının uzerine sayi eklenmesini saglamıs oluyoruz.
+			//Console.WriteLine("Artirma islemi:" + sayi);
+
+			//int sayi2 = 20;
+			//sayi2--; // - isareti ile var olan sayinin uzerinden sayi cikartilmasini saglamis oluyoruz.
+			//Console.WriteLine("Azaltma islemi:" + sayi2);
+			#endregion
+
+			#region atama operator islemi
+
+			#endregion
+			Console.ReadLine();
+        }
+    }
+}
