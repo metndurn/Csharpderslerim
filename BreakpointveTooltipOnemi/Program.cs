272a24d59ba6c0ba10742e8bf8a1bae90d08a21e
metndurn <mtndrn123@gmail.com>
2025-10-27 00:21:06 +0300

    kullanıcıdan deger almayı gorduk

---
+++ BreakpointveTooltipOnemi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakpointveTooltipOnemi
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region breakpoint ve kullanimi
+			// breakpoint kodun belirli bir satirinda uygulamanin durmasini saglayan bir aracdir.
+			//int sayi1 = 10;
+			//int sayi2 = 20;
+			//int sayi3 = 30;
+			//int toplam = sayi1 + sayi2 + sayi3;
+			//Console.WriteLine("Toplam Sonucu:" + toplam);
+			#endregion
+
+			Console.WriteLine("Breakpoint ve Tooltip Onemi");
+			/*tooltıp kısmı yazılan classlarda yada bır metodda acıklama gıbı bır yer acılır onun adı 
+			 * tooltıp'tır*/
+			Console.ReadLine();
+		}
+	}
+}
---
+++ KullanicidanDegerAlma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullanicidanDegerAlma
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// ReadLine() metodu kullanicidan deger almayi saglar.
+			Console.WriteLine("Lutfen adinizi giriniz:");
+			string isim = Console.ReadLine();
+
+			Console.WriteLine("Soyadinizi giriniz:");
+			string soyisim = Console.ReadLine();
+
+			Console.WriteLine("Merhaba " + isim + " " + soyisim + " MDAJANS'A Hoş Geldiniz..:");
+			Console.ReadLine();
+		}
+	}
+}
